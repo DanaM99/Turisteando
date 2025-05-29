b06e25dcf5a0d287857d5ed3432b7b23bfeb3176
rocioluque <rocioluq4@gmail.com>
2025-05-29 19:17:47 -0300

    Primer prueba de detalles de lugares

--- app/src/main/java/com/example/turisteando/detalles_lugares_activity.kt
+++ app/src/main/java/com/example/turisteando/detalles_lugares_activity.kt
@@ -1,6 +1,8 @@
 package com.example.turisteando
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,24 @@ class detalles_lugares_activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val lugar = intent.getStringExtra("lugar")
+
+        when (lugar) {
+            "paraiso" -> {
+                findViewById<TextView>(R.id.txtTitulo).text = "Parque Natural El Paraíso"
+                findViewById<ImageView>(R.id.imgLugar).setImageResource(R.drawable.parque_paraiso)
+                findViewById<TextView>(R.id.txtDescripcion).text =
+                    "Un espacio natural ideal para disfrutar de caminatas, aire puro y tranquilidad en Villa del Rosario."
+            }
+
+            "termas" -> {
+                findViewById<TextView>(R.id.txtTitulo).text = "Termas Villa del Rosario"
+                findViewById<ImageView>(R.id.imgLugar).setImageResource(R.drawable.villa)
+                findViewById<TextView>(R.id.txtDescripcion).text =
+                    "Un complejo termal reconocido por sus aguas curativas, piscinas y espacios de relax en contacto con la naturaleza."
+            }
+        }
+
     }
 }

cf26a2c60444380819b6f631be9828b74cc43d67
danaM99 <dana.moreno99@hotmail.com>
2025-05-30 17:21:19 -0300

    Se agregaron cinco lugares

--- app/src/main/java/com/example/turisteando/lugares_activity.kt
+++ app/src/main/java/com/example/turisteando/lugares_activity.kt
@@ -1,6 +1,8 @@
 package com.example.turisteando
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,36 @@ class lugares_activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnBasilica = findViewById<Button>(R.id.btnBasilica)
+        btnBasilica.setOnClickListener {
+            val intent = Intent(this, detalles_lugares_activity::class.java)
+            intent.putExtra("idLugar", 1)
+            startActivity(intent)
+        }
+
+        val btnCapilla = findViewById<Button>(R.id.btnCapilla)
+        btnCapilla.setOnClickListener {
+            val intent = Intent(this, detalles_lugares_activity::class.java)
+            intent.putExtra("idLugar", 2)
+            startActivity(intent)
+        }
+
+        val btnCementerio = findViewById<Button>(R.id.btnCementerio)
+        btnCementerio.setOnClickListener {
+            val intent = Intent(this, detalles_lugares_activity::class.java)
+            intent.putExtra("idLugar", 3)
+            startActivity(intent)
+        }
+
+        val btCvar = findViewById<Button>(R.id.btnCvar)
+        btnCementerio.setOnClickListener {
+            val intent = Intent(this, detalles_lugares_activity::class.java)
+            intent.putExtra("idLugar", 4)
+            startActivity(intent)
+        }
+
+
+
     }
 }
